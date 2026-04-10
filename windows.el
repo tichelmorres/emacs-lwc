8be4fc1b71be3478b9de94e99063d129127e6056
Michel Torres <qualquercoisahhhh@gmail.com>
2026-04-10 14:31:56 -0300

    att

--- windows.el
+++ windows.el
@@ -10,8 +10,10 @@
               (setq-local org-link-elisp-confirm-function nil)
               (setq-local visual-fill-column-width 40)
               (visual-line-mode 1)
-              (read-only-mode 1)
+              (read-only-mode 0)
               (visual-fill-column-mode 1)
+              (goto-char (point-min))
+              (forward-line 5)
               (let ((win (get-buffer-window (current-buffer) t)))
                 (when win (with-selected-window win (recenter (/ (window-body-height) 2))))))))
 
